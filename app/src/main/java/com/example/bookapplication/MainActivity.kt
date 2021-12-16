cf316ff187ced7c1aa12b280c9a69d21712456e3
Yeison Monsalve <81596987+yeison1503@users.noreply.github.com>
2021-12-16 01:13:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bookapplication/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.bookapplication
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bookapplication.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding : ActivityMainBinding
+    private var cal = Calendar.getInstance()
+    private var publicationDate = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener{ _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_YEAR, dayOfMonth)
+
+            val format = "dd-MM-yyyy"
+            val simpleDateFormat = SimpleDateFormat(format, Locale.US)
+            publicationDate = simpleDateFormat.format(cal.time).toString()
+            mainBinding.publicationDateButton.text = publicationDate
+        }
+
+        with(mainBinding) {
+
+            publicationDateButton.setOnClickListener{
+                DatePickerDialog(
+                    this@MainActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
+            saveButton.setOnClickListener {
+                if (nameBookEditText.text?.isEmpty() == true ||
+                    nameAuthorEditText.text?.isEmpty() == true ||
+                    pagesEditText.text?.isEmpty() == true
+                ) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Debe digitar nombre, autor y número de páginas",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val nameBook = nameBookEditText.text.toString()
+                    val author = nameAuthorEditText.text.toString()
+                    val pages = pagesEditText.text.toString().toInt()
+                    val abstract = abstractEditText.text.toString()
+
+                    var genre = ""
+                    if (suspenseCheckBox.isChecked) genre = "Suspenso"
+                    if (terrorCheckBox.isChecked) genre += "Terror"
+                    if (infantileCheckBox.isChecked) genre += "Infantil"
+                    if (fictionCheckBox.isChecked) genre += "Ficción"
+
+                   // var score = if (oneRadioButton.isChecked) 1 else 2
+                    val score = when {
+                        oneRadioButton.isChecked -> 1
+                        twoRadioButton.isChecked -> 2
+                        threeRadioButton.isChecked -> 3
+                        fourRadioButton.isChecked -> 4
+                        else -> 5
+                    }
+
+
+
+                    infoTextView.text = getString(R.string.info, nameBook, author, pages, abstract, genre, score, publicationDate)
+                }
+            }
+        }
+    }
+}
